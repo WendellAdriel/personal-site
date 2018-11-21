1af5004148e6959e007b33ed125ef94093cc5300
Wendell Adriel <wendelladriel.ti@gmail.com>
2018-11-21 14:53:39 +0000

    finished about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,8 +33,7 @@ export default () => (
         margin-right: 3rem;
 
         @media (max-width: 767px) {
-          margin-top: 2rem;
-          margint-right: 0;
+          margin-right: 0;
         }
       `}>
         <ProfileImage src={profileImage} alt="Profile Image"></ProfileImage>
@@ -53,6 +52,25 @@ export default () => (
           many technologies like: HTML, CSS, JavaScript (Front-end and Back-end), PHP, Java, Go, SQL and NoSQL databases,
           Docker and much more, but my main stack is PHP and JS (Front-end and Back-end).
         </p>
+
+        <h3>A little more about my work</h3>
+        <ul>
+          <li>I casually write articles on Medium, technical or not, in english or in portuguese;</li>
+          <li>I have some Open Source Projects that you can check on my Github Profile;</li>
+          <li>I already led some talks and some workshops about software development;</li>
+          <li>
+            I like to participate in developer groups to talk about development, methodologies, etc.
+            For this I use mainly Telegram, Slack and Facebook;
+          </li>
+        </ul>
+
+        <h3>Some of my hobbies</h3>
+        <ul>
+          <li>I <strong>LOVE</strong> to play tabletop games, especially RPGs and I'm a Dungeon Master since 2008;</li>
+          <li>I casually play the drums and bass guitar. I'm not good at it, I do it just for fun xD;</li>
+          <li>I really like to watch movies or series with my wife (even better while eating some popcorn);</li>
+          <li>I like to create board/card games, I just do it for fun and to play with friends;</li>
+        </ul>
       </div>
     </Section>
   </Layout>
